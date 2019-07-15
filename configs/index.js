45fe18d3639464b1232a9982685cd784bf3ede5a
Maxwell <maxwell88soltyk@gmail.com>
2019-07-15 08:01:47 +0300

    add config file

---
+++ configs/index.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+module.exports = {
+    host      : "0.0.0.0",
+    port      : 3000,
+    apiUrl    : "https://starnavi-frontend-test-task.herokuapp.com",
+    apiPrefix : ""
+};
